203594082a07550942cec46c338068e617f63cab
Richard Li <a13323600@gmail.com>
2023-01-14 14:01:07 +1300

    Delete cursor placement (#10)
    And fixed enter_insert() post_nav nil but callback non nil
    changed enter_insert() signiture to make more sense

--- lua/term-edit/delete.lua
+++ lua/term-edit/delete.lua
@@ -2,6 +2,7 @@ local utils = require 'term-edit.utils'
 -- local coord = require 'term-edit.coord'
 local insert = require 'term-edit.insert'
 local navigate = require 'term-edit.navigate'
+local async = require 'term-edit.async'
 local M = {}
 
 local function delete_keys(len)
@@ -14,8 +15,9 @@ end
 ---Start in
 ---@param start Coord
 ---@param end_ Coord
----@param callback? function
-function M.delete_range(start, end_, callback)
+---@param opts? { callback?: function, post_nav?: integer }
+function M.delete_range(start, end_, opts)
+  opts = opts or {}
   utils.debug_print(
     'delete_range: start:',
     utils.inspect(start),
@@ -24,14 +26,23 @@ function M.delete_range(start, end_, callback)
   )
 
   local function delete()
-    navigate.navigate_with(start, delete_keys, callback)
+    navigate.navigate_with(start, delete_keys, function()
+      if opts.post_nav then
+        async.feedkeys(
+          insert.move_keys(opts.post_nav),
+          opts.callback,
+          { moves = true }
+        )
+      elseif opts.callback then
+        opts.callback()
+      end
+    end)
   end
 
-  insert.enter_insert {
-    target = end_,
+  insert.enter_insert(end_, {
     callback = delete,
     post_nav = 1,
-  }
+  })
 end
 
 return M
--- lua/term-edit/insert.lua
+++ lua/term-edit/insert.lua
@@ -5,7 +5,7 @@ local M = {}
 
 ---move right by len
 ---@param len integer negative mean move left
-local function move_keys(len)
+function M.move_keys(len)
   utils.debug_print('move_by: ', len)
   if len > 0 then
     return string.rep('<Right>', len)
@@ -17,17 +17,20 @@ local function move_keys(len)
 end
 
 ---Enter insert mode
----@param opts { callback?: function, post_nav?: integer, target: Coord }
-function M.enter_insert(opts)
-  utils.debug_print('enter_insert: target: ', utils.inspect(opts.target))
+---@param opts? { callback?: function, post_nav?: integer }
+function M.enter_insert(target, opts)
+  opts = opts or {}
+  utils.debug_print('enter_insert: target: ', utils.inspect(target))
   async.vim_cmd('startinsert', function()
-    navigate.navigate_with(opts.target, move_keys, function()
+    navigate.navigate_with(target, M.move_keys, function()
       if opts.post_nav then
         async.feedkeys(
-          move_keys(opts.post_nav),
+          M.move_keys(opts.post_nav),
           opts.callback,
           { moves = true }
         )
+      elseif opts.callback then
+        opts.callback()
       end
     end)
   end)
