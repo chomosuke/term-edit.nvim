86016b92e84874d76977c747ab2af55230c1d888
chomosuke <a13323600@gmail.com>
2023-01-18 22:30:21 +1300

    feedkeys_delay more conservertive

--- lua/term-edit/config.lua
+++ lua/term-edit/config.lua
@@ -2,26 +2,24 @@ local M = {
   ---@type TermEditOpts
   opts = {
     debug = false,
-    feedkeys_delay = 3,
+    feedkeys_delay = 10,
   },
 }
 
 ---@class TermEditOpts
 ---@field prompt_start string
 ---@field debug boolean
----@field key_queue_time integer
+---@field feedkeys_delay integer
 
 ---set options for term-edit
 ---@param opts TermEditOpts
 function M.setup(opts)
   if not opts or not opts.prompt_start then
     vim.notify(
-      [[
-prompt_start is a mandatory argument to setup!
+      [[prompt_start is a mandatory argument to setup!
 Please provide it with a lua pattern that would match the end of the shell prompt.
 Say if your shell prompt is `user@Host ~ $ ` then do:
-`require('term-edit').setup { prompt_start = ' %$ ' }`
-    ]],
+`require('term-edit').setup { prompt_start = ' %$ ' }`]],
       vim.log.levels.WARN
     )
   end
